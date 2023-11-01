c32646cc5146ad3f996554e26ca4611e759249ec
necipsunbul <ns@necipsunbul.com>
2023-11-01 20:43:53 +0300

    image upload middleware wrapped with try exception

--- src/middlewares/AuthMid.ts
+++ src/middlewares/AuthMid.ts
@@ -1,2 +1,4 @@
 import { Request, Response, NextFunction } from "express";
-export default (req: Request, res: Response, next: NextFunction) => {};
+export default (req: Request, res: Response, next: NextFunction) => {
+  next();
+};
--- src/middlewares/ImageUploadMid.ts
+++ src/middlewares/ImageUploadMid.ts
@@ -6,25 +6,31 @@ import ErrorResponse from "../libs/core/response/ErrorResponse";
 import httpStatus from "http-status";
 export default (req: Request, res: Response, next: NextFunction) =>
   async (field: string, amount: number = 1) => {
-    if (!req.files) return false;
-    const uploader = new FileUploadManager(
-      req.files,
-      ImageFileContants.MimeTypes,
-      ImageFileContants.Extensions,
-      FileManager.getPath(["public", "images"])
-    );
-    let result: Boolean = false;
-    if (amount === 1) {
-      result = await uploader.saveSingleFileToServer(field);
-      if (result) req.uploadedFile = uploader.uploadedFile;
-    } else {
-      uploader.maxFileCount = amount;
-      result = await uploader.saveAllFilesToServer(field);
-      if (result) req.uploadedFiles = uploader.uploadedFiles;
-    }
-    if (!result)
-      return res.json(
-        new ErrorResponse(uploader.error.message, httpStatus.BAD_REQUEST)
+    try {
+      if (!req.files) return false;
+      const uploader = new FileUploadManager(
+        req.files,
+        ImageFileContants.MimeTypes,
+        ImageFileContants.Extensions,
+        FileManager.getPath(["public", "images"])
       );
-    next();
+      let result: Boolean = false;
+      if (amount === 1) {
+        result = await uploader.saveSingleFileToServer(field);
+        if (result) req.uploadedFile = uploader.uploadedFile;
+      } else {
+        uploader.maxFileCount = amount;
+        result = await uploader.saveAllFilesToServer(field);
+        if (result) req.uploadedFiles = uploader.uploadedFiles;
+      }
+      if (!result)
+        return res.json(
+          new ErrorResponse(uploader.error.message, httpStatus.BAD_REQUEST)
+        );
+      next();
+    } catch (e) {
+      if (e instanceof Error) {
+        throw new Error(e.message);
+      }
+    }
   };
