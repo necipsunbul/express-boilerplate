8e2cc701fd89dd9624ece3868b3be8b7d4c332fa
necipsunbul <ns@necipsunbul.com>
2023-11-14 18:29:51 +0300

    upload middleware param name fixed =>  amount to filecount

--- src/middlewares/ImageUploadMid.ts
+++ src/middlewares/ImageUploadMid.ts
@@ -5,7 +5,7 @@ import FileManager from "../libs/managers/FileManager";
 import ErrorResponse from "../libs/core/response/ErrorResponse";
 import httpStatus from "http-status";
 export default (req: Request, res: Response, next: NextFunction) =>
-  async (field: string, amount: number = 1) => {
+  async (field: string, fileCount: number = 1) => {
     try {
       if (!req.files) return false;
       const uploader = new FileUploadManager(
@@ -15,11 +15,11 @@ export default (req: Request, res: Response, next: NextFunction) =>
         FileManager.getPath(["public", "images"])
       );
       let result: Boolean = false;
-      if (amount === 1) {
+      if (fileCount === 1) {
         result = await uploader.saveSingleFileToServer(field);
         if (result) req.uploadedFile = uploader.uploadedFile;
       } else {
-        uploader.maxFileCount = amount;
+        uploader.maxFileCount = fileCount;
         result = await uploader.saveAllFilesToServer(field);
         if (result) req.uploadedFiles = uploader.uploadedFiles;
       }
