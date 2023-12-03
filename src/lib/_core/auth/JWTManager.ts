251e0d3bcfe0c421e83ba609441b4e3b48e5fb6c
necipsunbul <ns@necipsunbul.com>
2023-12-03 16:15:37 +0300

    JWT manager configured

--- src/lib/_core/auth/JWTManager.ts
+++ src/lib/_core/auth/JWTManager.ts
@@ -1,29 +1,29 @@
-import jwt,{ SignOptions,Algorithm } from 'jsonwebtoken';
-import {vercelMs} from "../constants/appConstants";
+import jwt, { SignOptions, Algorithm } from "jsonwebtoken";
+import { vercelMs } from "../constants/appConstants";
 
 export class JWTManager {
-    private static instance: JWTManager;
-    private constructor() { }
-    public static get getInstance(): JWTManager {
-        if (!JWTManager.instance) JWTManager.instance = new JWTManager();
-        return JWTManager.instance;
-    }
+  private static instance: JWTManager;
+  private constructor() {}
+  public static get getInstance(): JWTManager {
+    if (!JWTManager.instance) JWTManager.instance = new JWTManager();
+    return JWTManager.instance;
+  }
 
-    private algorithm : Algorithm = 'HS384';
+  private algorithm: Algorithm = "HS384";
 
-    async sign(payload:string ,secretKey:string, expireTime?: vercelMs) : Promise<string> {
-       try{
-           const options : SignOptions = {
-               expiresIn: expireTime,
-               algorithm: this.algorithm
-           };
-           return await jwt.sign(payload,secretKey,options);
-       }catch (e) {
-           throw new Error("JWT sign error");
-       }
+  async sign(payload: string, secretKey: string, expireTime?: vercelMs) {
+    try {
+      const options: SignOptions = {
+        expiresIn: expireTime,
+        algorithm: this.algorithm,
+      };
+      return jwt.sign(payload, secretKey, options);
+    } catch (e) {
+      throw new Error("JWT sign error");
     }
+  }
 
-    verify(token:string ,secretKey:string){
-       return jwt.verify(token, secretKey);
-    }
+  verify(token: string, secretKey: string) {
+    return jwt.verify(token, secretKey);
+  }
 }
