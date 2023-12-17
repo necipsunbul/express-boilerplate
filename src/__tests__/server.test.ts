7716c4a1b241de43e018e63bcfc5ac6c65ef834e
necipsunbul <ns@necipsunbul.com>
2023-12-17 16:44:17 +0300

    server.test.ts configured

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,12 +1,7 @@
 import { agent as request } from "supertest";
-import { appConfigs } from "../lib/_core/config/config";
 import { closeDatabases } from "../lib/_core/config/config";
 import app from "../index";
 describe("Default endpoint => '/' tests", () => {
-  beforeAll(async () => {
-    await appConfigs();
-  });
-
   test("Catch home route", async () => {
     const res = await request(app).get("/");
     const responseData = {
